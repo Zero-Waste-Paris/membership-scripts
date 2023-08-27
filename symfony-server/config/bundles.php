77279a8eb7b65f18835794203c6e37ed1df2a87c
Guillaume Turri <g.turri@criteo.com>
2023-08-27 20:50:51 +0200

    Add authent'
    With this commit:
    
    - /api endpoints are allowed only for user with ROLE_USER role
    - so when a user points his or her browser to a /api endpoint, he or she
      is auto-redirected to route app_login, on /login (http 302)
    - without valid credentials, this route displays a login form
    - along the way, the user has a php session id cookie
    - when the user submit valid credentials, the FormLogin authenticator
      update the server-side PHP session, and the user is redirected to the
      page requested in the first place.
    - eventually the user can log out with a POST to /logout
    
    A few open questions at this step are:
    - could we / should we get a 401 (instead of 302) when an unauthenticated
      user tries to get an endpoint with ACL (so the angular app would know
      that it needs to perform an authent' rather than having to "guess" from
      the 302)
    - how should the angular client perform the authent' query without hardcoding
      the fields expected by the LoginFormAuthenticator
    - Should we worry about CSRF; and if yes, how should we share a csrf token
      between the Symfony back and the Angular front

--- symfony-server/config/bundles.php
+++ symfony-server/config/bundles.php
@@ -10,4 +10,7 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
