6923d1e5910e7196658105025d5d9a93f577fd60
Guillaume Turri <guillaume.turri@gmail.com>
2024-01-18 15:23:09 +0100

    [angular] use date pipe instead of ad hoc code
    this simplifies a bit the code, getting rid of a field

--- angular-front/src/app/slack-outdated/slack-outdated.component.ts
+++ angular-front/src/app/slack-outdated/slack-outdated.component.ts
@@ -2,18 +2,17 @@ import { Component } from '@angular/core';
 import { TimestampedSlackUserList } from '../generated/api/model/timestampedSlackUserList';
 import { DataProviderService } from '../data-provider.service';
 import { DefaultService } from '../generated/api/api/default.service';
-import { NgIf, NgFor } from '@angular/common';
+import { NgIf, NgFor, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-slack-outdated',
   standalone: true,
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, DatePipe],
   templateUrl: './slack-outdated.component.html',
   styleUrl: './slack-outdated.component.css'
 })
 export class SlackOutdatedComponent {
 	data: TimestampedSlackUserList|null = null;
-	dataFreshnessAsString: string|null = null;
 
 	constructor(
 		dataProvider: DataProviderService,
@@ -22,7 +21,6 @@ export class SlackOutdatedComponent {
 		let self = this;
 		promise.then(data => {
 			self.data = data;
-			self.dataFreshnessAsString = new Date(data.timestamp*1000).toISOString();
 		});
 	}
 }
