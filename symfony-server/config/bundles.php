d5fc149c4c3ba5e4237c7e74596fb327ce9a534b
Guillaume Turri <g.turri@criteo.com>
2025-04-21 07:24:15 +0200

    rancomposer require 2fa
    Following doc from
    https://symfony.com/bundles/SchebTwoFactorBundle/current/installation.html

--- symfony-server/config/bundles.php
+++ symfony-server/config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
 ];
