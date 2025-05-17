978fffef60cbd6a91a1c7160709d9f1d45587c18
Guillaume Turri <g.turri@criteo.com>
2025-05-17 22:34:00 +0200

    Add skeleton of totp component

---
+++ angular-front/src/app/totp/totp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-totp',
+  standalone: true,
+  imports: [],
+  templateUrl: './totp.component.html',
+  styleUrl: './totp.component.css'
+})
+export class TotpComponent {
+
+}
