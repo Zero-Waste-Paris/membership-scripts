f04034f611721d5a17e3f647f8a203b87e54704c
Guillaume Turri <g.turri@criteo.com>
2025-05-19 21:16:42 +0200

    Add missing import

--- symfony-server/src/Command/UserDisableTotp.php
+++ symfony-server/src/Command/UserDisableTotp.php
@@ -21,6 +21,7 @@ namespace App\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use App\Repository\UserRepository;
 
