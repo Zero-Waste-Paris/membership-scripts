0f4934ac29a4f0b053efba84695f80d696e1dfe2
g.turri <g.turri@criteo.com>
2025-11-23 21:42:24 +0100

    install and launch wiremock standalone

--- scripts/runSymfonyTests.sh
+++ scripts/runSymfonyTests.sh
@@ -8,11 +8,25 @@ if [ -L "$0" ] && [ -x $(which readlink) ]; then
 else
   THIS_FILE="$0"
 fi
-SCRIPT_DIR="$(dirname "$THIS_FILE")"
+SCRIPT_DIR="$( realpath "$(dirname "$THIS_FILE")")"
 
-cd "$SCRIPT_DIR"/../symfony-server/
 
 export APP_ENV=test
+
+WIREMOCK_VERSION=3.13.2
+BIN_DIR="$SCRIPT_DIR/../bin"
+WIREMOCK_JAR="$BIN_DIR/wiremock.$WIREMOCK_VERSION.jar"
+WIREMOCK_PORT=8081
+mkdir -p "$BIN_DIR"
+if [ ! -e "$WIREMOCK_JAR" ]; then
+  echo "Downloading wiremock-standalone jar"
+  curl "https://repo1.maven.org/maven2/org/wiremock/wiremock-standalone/$WIREMOCK_VERSION/wiremock-standalone-$WIREMOCK_VERSION.jar" > "$WIREMOCK_JAR"
+fi
+
+curl http://localhost:$WIREMOCK_PORT/__admin/shutdown || true # try to kill an old process if any
+java -jar "$WIREMOCK_JAR" --port "$WIREMOCK_PORT" &
+
+cd "$SCRIPT_DIR"/../symfony-server/
 
 echo "Creating the test database"
 php bin/console --env=test doctrine:database:create
